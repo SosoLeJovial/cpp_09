f2ef1715d14d210c9a9ba9e60464fa20601e30a4
SosoLeJovial <sofien.tebourbi@gmail.com>
2025-08-03 16:58:49 +0200

    clean

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tsofien- <tsofien-@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 09:58:51 by tsofien-          #+#    #+#             */
-/*   Updated: 2025/08/02 22:49:05 by tsofien-         ###   ########.fr       */
+/*   Updated: 2025/08/03 16:58:03 by tsofien-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int main(int ac, char **av)
 {
 	if (ac <= 1)
 	{
-		std::cout << RED << "Usage: " << av[0] << RESET << std::endl;
+		std::cout << RED << "Usage: " << av[0] << " \"[Int ...]\"" << RESET << std::endl;
 		return 1;
 	}
 
